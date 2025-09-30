f17c9937cb71ab81ab453a3c52217741cdd4cf5c
Lalit Rai <lalitrai860@gmail.com>
2025-09-30 14:04:52 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Lalit Rai</h1>
+        <p className={styles.description}>
+          I am a passionate Java Full Stack Developer skilled in creating dynamic and user-friendly web applications. With expertise in frontend, backend, and database integration, I focus on delivering efficient, scalable, and high-quality solutions to meet client needs.
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/LalitLogo.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
